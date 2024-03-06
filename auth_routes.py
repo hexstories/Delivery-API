b98557547373709c405ace7217bbe4fcbdc25b26
hexstories <kwakuasihene@gmail.com>
2024-03-06 03:46:42 +0000

    auth

--- auth_routes.py
+++ auth_routes.py
@@ -2,6 +2,9 @@ from fastapi import APIRouter
 from database import Session, engine
 from schema import SignUpModel
 from models import User
+from fastapi.exceptions import HTTPException
+from werkzeug.security import generate_password_hash,check_password_hash
+
 
 auth_router = APIRouter(
     prefix="/auth",
@@ -14,10 +17,32 @@ session = Session(bind=engine)
 async def hello():
     return {"message": "Hello World"}
 
-@auth_router.post('/signup')
+@auth_router.post('/signup',
+    status_code=201)
 async def signup(user: SignUpModel):
     db_email = session.query(User).filter(User.email == user.email).first()
+
+    if db_email is not None:
+        raise HTTPException(status_code=400, detail="Email already exists")
+
+    db_username = session.query(User).filter(User.email == user.username).first()
+
+    if db_email is not None:
+        raise HTTPException(status_code=400, detail="Username already exists")
     
+    new_user = User(
+        username = user.username,
+        email = user.email,
+        password = generate_password_hash(user.password),
+        is_active = user.is_active,
+        is_staff = user.is_staff
+    )
+
+    session.add(new_user)
+
+    session.commit()
+
+    return new_user
 
 
     
